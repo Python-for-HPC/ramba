4f2b9ac00b9039b8cf85f067f156ee553ab57d20
Todd A. Anderson <drtodd13@comcast.net>
2022-01-20 14:38:47 -0800

    Fix comparator for 0d xarray sum.

--- ramba/tests/test_xarray.py
+++ ramba/tests/test_xarray.py
@@ -3,29 +3,33 @@ import numpy as np
 import random
 import numba
 import pytest
+import math
 
 xarray = pytest.importorskip("xarray")
 
 
-def rb_comparer(np_res, ramba_res, array_comp):
+def rb_comparer(np_res, ramba_res, comp):
     if isinstance(np_res, tuple):
         assert isinstance(ramba_res, tuple)
         assert len(np_res) == len(ramba_res)
         for i in range(len(np_res)):
-            rb_comparer(np_res[i], ramba_res[i], array_comp)
+            rb_comparer(np_res[i], ramba_res[i], comp)
     elif isinstance(np_res, np.ndarray):
         assert isinstance(ramba_res, ramba.ndarray)
         ramba_local = ramba_res.asarray()
-        assert array_comp(np_res, ramba_local)
+        assert comp(np_res, ramba_local)
+    elif isinstance(np_res, float):
+        assert isinstance(ramba_res, float)
+        return comp (np_res, ramba_res)
     else:
         assert np_res == ramba_res
 
 
-def run_both(func, *args, array_comp=np.array_equal):
+def run_both(func, *args, comp):
     ramba_res = func(ramba, *args)
     ramba.sync()
     np_res = func(np, *args)
-    rb_comparer(np_res, ramba_res, array_comp)
+    rb_comparer(np_res, ramba_res, comp)
 
 
 class TestXarray:
@@ -38,6 +42,6 @@ class TestXarray:
             xa3 = np.sin(xa22)
             xa4 = xa3.transpose()
             xa5 = xa4.sum()
-            return xa5
+            return xa5.data.item()
 
-        run_both(impl)
+        run_both(impl, comp=math.isclose)
